dbfc1ffc13901bef1efb93ef0181c791504fa548
LLehtola <56760014+LLehtola@users.noreply.github.com>
2021-02-09 15:25:23 +0200

    build: initial build
    Initial build

---
+++ src/main/java/com/experis/movie_character_api/MovieCharacterApiApplication.java
@@ -0,0 +1,13 @@
+package com.experis.movie_character_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCharacterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCharacterApiApplication.class, args);
+	}
+
+}
