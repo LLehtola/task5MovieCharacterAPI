d29e46b0152fdcbb313557c7c4ebd9dd1f093bde
maija.haka <maija.haka@5CG05206QX.nordic.local>
2021-02-09 21:13:21 +0200

    feat(model): add basic fields to character model

--- src/main/java/com/experis/movie_character_api/models/Character.java
+++ src/main/java/com/experis/movie_character_api/models/Character.java
@@ -1,5 +1,13 @@
 package com.experis.movie_character_api.models;
 
+// Lombok
 public class Character {
 
+    Long id; // auto-incremented, AbstractPersistable<Long>?
+    String fullName;
+    String alias; // if applicable
+    String gender; // or enum?
+    String pictureUrl;
+
+    // relationships
 }
