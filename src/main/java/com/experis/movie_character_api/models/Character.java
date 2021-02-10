164e00b778f817bf53289c3bfeb34b37a8bd5eaf
maija.haka <maija.haka@5CG05206QX.nordic.local>
2021-02-10 19:01:10 +0200

    feat(model): define character entity

--- src/main/java/com/experis/movie_character_api/models/Character.java
+++ src/main/java/com/experis/movie_character_api/models/Character.java
@@ -1,13 +1,31 @@
 package com.experis.movie_character_api.models;
 
-// Lombok
-public class Character {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 
-    Long id; // auto-incremented, AbstractPersistable<Long>?
-    String fullName;
-    String alias; // if applicable
-    String gender; // or enum?
-    String pictureUrl;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
-    // relationships
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Data
+public class Character extends AbstractPersistable<Long> {
+
+    private String fullName;
+    private String alias; // if applicable
+    private Gender gender;
+    private String pictureUrl;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "characters")
+    private List<Movie> movies = new ArrayList<>();
+}
+
+enum Gender {
+    male,
+    female,
+    other
 }
