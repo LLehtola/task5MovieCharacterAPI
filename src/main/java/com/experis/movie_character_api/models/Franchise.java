f20603346dc7d78bc9e24d22f380adfa9dce4e9a
lauri.lehtola <mr.lauri.lehtola@gmail.com>
2021-02-10 14:49:05 +0200

    Initial version of Franchise model class

--- src/main/java/com/experis/movie_character_api/models/Franchise.java
+++ src/main/java/com/experis/movie_character_api/models/Franchise.java
@@ -1,4 +1,23 @@
 package com.experis.movie_character_api.models;
 
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Franchise {
+
+    private Long id;
+    private String name;
+    private String description;
+
+    public Franchise(String name, String description){
+        this.id+=1;
+        this.name=name;
+        this.description=description;
+    }
+
 }
