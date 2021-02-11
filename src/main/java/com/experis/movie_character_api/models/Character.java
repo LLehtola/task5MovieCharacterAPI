7629492a373fef331025dd4ef5851bd83c3de4b3
maija.haka <maija.haka@5CG05206QX.nordic.local>
2021-02-11 16:54:48 +0200

    feat(repository): create CharacterRepository allowing basic CRUD operations

--- src/main/java/com/experis/movie_character_api/models/Character.java
+++ src/main/java/com/experis/movie_character_api/models/Character.java
@@ -5,9 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,13 +15,18 @@ public class Character extends AbstractPersistable<Long> {
 
     private String fullName;
     private String alias; // if applicable
+
+    @Enumerated(EnumType.ORDINAL)
     private Gender gender;
+
     private String pictureUrl;
 
     @ManyToMany(fetch = FetchType.LAZY, mappedBy = "characters")
     private List<Movie> movies = new ArrayList<>();
 }
 
+// Hibernate maps Gender enum to its ordinal value in the enum class,
+// so do not change the order of genders in the class
 enum Gender {
     male,
     female,
--- src/main/java/com/experis/movie_character_api/repositories/CharacterRepository.java
+++ src/main/java/com/experis/movie_character_api/repositories/CharacterRepository.java
@@ -1,4 +1,9 @@
 package com.experis.movie_character_api.repositories;
 
-public class CharacterRepository {
+import com.experis.movie_character_api.models.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
 }
