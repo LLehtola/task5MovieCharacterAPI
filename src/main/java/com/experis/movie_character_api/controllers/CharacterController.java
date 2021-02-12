121ab0157d4ed14a3d25951a6abe69034d7c7a3f
maija.haka <maija.haka@5CG05206QX.nordic.local>
2021-02-12 13:18:38 +0200

    feat(controller): add Character controller with basic CRUD operations

--- src/main/java/com/experis/movie_character_api/controllers/CharacterController.java
+++ src/main/java/com/experis/movie_character_api/controllers/CharacterController.java
@@ -1,4 +1,86 @@
 package com.experis.movie_character_api.controllers;
 
+import com.experis.movie_character_api.models.Character;
+import com.experis.movie_character_api.repositories.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/characters")
 public class CharacterController {
+
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Character>> getAllCharacters() {
+        List<Character> characters = characterRepository.findByIdNotNull();
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(characters, status);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Character> getCharacterById(@PathVariable Long id) {
+        HttpStatus status;
+
+        if (!characterRepository.existsById(id)) {
+            status = HttpStatus.NOT_FOUND;
+            return new ResponseEntity<>(null, status);
+        }
+
+        status = HttpStatus.OK;
+        Character returnedCharacter = characterRepository.findById(id).get();
+
+        return new ResponseEntity<>(returnedCharacter, status);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Character> addCharacter(@RequestBody Character character) {
+        HttpStatus status;
+        character = characterRepository.save(character);
+        status = HttpStatus.CREATED;
+        return new ResponseEntity<>(character, status);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Character> updateCharacter(@PathVariable Long id,
+                                                     @RequestBody Character character) {
+        HttpStatus status;
+
+        if (!id. equals(character.getId())) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(null, status);
+        }
+
+        if (!characterRepository.existsById(id)) {
+            status = HttpStatus.NOT_FOUND;
+            return new ResponseEntity<>(null, status);
+        }
+
+        Character returnedCharacter = characterRepository.save(character);
+        status = HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(returnedCharacter, status);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCharacter(@PathVariable Long id) {
+        HttpStatus status;
+
+        if (!characterRepository.existsById(id)) {
+            status = HttpStatus.NOT_FOUND;
+            return new ResponseEntity<>("requested resource was not found",
+                    status);
+        }
+
+        Character character = characterRepository.findById(id).get();
+        characterRepository.delete(character);
+        status = HttpStatus.OK;
+        return new ResponseEntity<>("resource with id " + id + " was deleted",
+                status);
+    }
 }
--- src/main/java/com/experis/movie_character_api/repositories/CharacterRepository.java
+++ src/main/java/com/experis/movie_character_api/repositories/CharacterRepository.java
@@ -1,9 +1,16 @@
 package com.experis.movie_character_api.repositories;
 
 import com.experis.movie_character_api.models.Character;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Long> {
+
+    // avoid N+1 query problem when fetching all characters
+    @EntityGraph(attributePaths = {"movies"})
+    List<Character> findByIdNotNull();
 }
